eab532d44cc20fa08a7a03053b15356d456e13a1
Udit Manektala <code@getbraintree.com>
2016-03-02 18:59:16 -0800

    webpackingshit

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
 module.exports = {
   entry: [
     './app/index.js'
-  ]
+  ],
   module: {
     loaders: [
       {test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
