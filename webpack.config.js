ceba37492e914b1afb09a98aa7b78b02b7c5aa28
Udit Manektala <code@getbraintree.com>
2016-03-02 18:38:38 -0800

    Add coffee loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,13 @@ odule.exports = {
   entry: [
     './app/index.js'
   ]
+  module: {
+    loaders: [
+      {test: /\.coffee$/, exclude: /node_modules/, loader: "coffee-loader"}
+    ]
+  },
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
 }
